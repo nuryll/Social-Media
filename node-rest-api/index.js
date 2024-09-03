96a4b19d9e7204ddee0ca2d28aaa05c88c513068
nuryll <52735279+nuryll@users.noreply.github.com>
2024-09-03 21:56:46 +0200

    Initial commit

---
+++ node-rest-api/index.js
@@ -0,0 +1,31 @@
+const express =  require("express"); 
+const app =  express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+//Connected MongoDB with .env file.
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Failed to connect to MongoDB:', err);
+    });
+
+
+    //Middleware
+    app.use(express.json());
+    app.use(helmet());
+    app.use(morgan("common"));
+
+    app.get("/",(req,res)=>{
+        res.send("Welcome to Homepage")
+    })
+
+app.listen(8800,()=>{
+    console.log("Backend server is running on port 8800");
+})
