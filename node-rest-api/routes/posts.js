b17a991acdfbbe60181656ef6f684f3341a3d2bf
nuryll <52735279+nuryll@users.noreply.github.com>
2024-09-11 12:12:19 +0200

    Update posts.js

--- node-rest-api/routes/posts.js
+++ node-rest-api/routes/posts.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
-const { findById } = require("../models/User");
+const User = require("../models/User");
 
 //create a post
 router.post("/", async(req,res)=>{
@@ -13,6 +13,7 @@ router.post("/", async(req,res)=>{
     }
 
 })
+
 //update a post
 
 router.put("/:id", async(req,res)=>{
@@ -29,6 +30,7 @@ router.put("/:id", async(req,res)=>{
     }
 
 });
+
 //delete a post
 router.delete("/:id", async(req,res)=>{
     try{
@@ -44,8 +46,48 @@ router.delete("/:id", async(req,res)=>{
     }
 
 });
-//like a post
+
+//like/dislike a post
+router.put("/:id/like", async (req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if(!post.likes.includes(req.body.userId)){
+            await post.updateOne({$push:{likes:req.body.userId}});
+            res.status(200).json("The post has been liked");
+        }else{
+            await post.updateOne({$pull:{likes:req.body.userId}});
+            res.status(200).json("The post has been disliked");
+        }
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
 //get a post
+router.get("/:id", async(req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
 //get timeline posts
+router.get("/timeline/all", async(req,res)=>{
+    try{
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({userId: currentUser._id});
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId) =>{
+                return Post.find({userId: friendId});
+            })
+        );
+        res.json(userPosts.concat(...friendPosts))
+    }catch(err){
+        res.status(500).json(err);
+    }
+
+});
 
 module.exports = router;
