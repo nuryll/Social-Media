a1f4b5115da892d0155514544fd41c9b4984f724
nuryll <52735279+nuryll@users.noreply.github.com>
2024-10-08 14:35:36 +0200

    Update Topbar.jsx

--- react-social/src/components/topbar/Topbar.jsx
+++ react-social/src/components/topbar/Topbar.jsx
@@ -5,7 +5,7 @@ export default function Topbar() {
   return (
     <div className="topbarContainer">
         <div className="topbarLeft">
-          <span className="logo">Social</span>
+          <span className="logo">Kaleidoscope</span>
         </div>
         <div className="topbarCenter">
           <div className="searchbar">
